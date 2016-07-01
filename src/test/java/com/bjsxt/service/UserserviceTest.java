d192515da811a536bf204202510ea2c322f8dead
Mark L Tao <mark.l.tao@noexternalmail.hsbc.com>
2016-07-01 17:35:59 +0800

    spring 08 class done!

--- src/test/java/com/bjsxt/service/UserserviceTest.java
+++ src/test/java/com/bjsxt/service/UserserviceTest.java
@@ -1,11 +1,10 @@
 package com.bjsxt.service;
 
-import com.bjsxt.dao.UserDAO;
 import com.bjsxt.model.User;
-import com.bjsxt.spring.BeanFactory;
-import com.bjsxt.spring.ClassPathXmlApplicationContext;
-
-import static org.junit.Assert.*;
+//import com.bjsxt.spring.BeanFactory;
+//import com.bjsxt.spring.ClassPathXmlApplicationContext;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Created by Mark on 2016/6/29.
@@ -14,7 +13,7 @@ public class UserserviceTest {
 
     @org.junit.Test
     public void add() throws Exception {
-        BeanFactory beanFactory = new ClassPathXmlApplicationContext();
+        BeanFactory beanFactory = new ClassPathXmlApplicationContext("beans.xml");
         Userservice userservice = (Userservice) beanFactory.getBean("userService");
 //        UserDAO userDAO = (UserDAO) beanFactory.getBean("u");
 //        userservice.setUserDAO(userDAO);
