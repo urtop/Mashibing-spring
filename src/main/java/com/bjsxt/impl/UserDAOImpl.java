5e381ce1017d99d1f5bc3b6652bd8f696862543e
Mark L Tao <mark.l.tao@noexternalmail.hsbc.com>
2016-07-13 10:44:44 +0800

    spring Ep 25,mysql insert works,use mysql-connector 5.1.39 insread of 5.1.9

--- src/main/java/com/bjsxt/impl/UserDAOImpl.java
+++ src/main/java/com/bjsxt/impl/UserDAOImpl.java
@@ -4,20 +4,44 @@ import com.bjsxt.dao.UserDAO;
 import com.bjsxt.model.User;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Random;
+
 /**
  * Created by Mark on 2016/6/29.
  */
 @Component("UserDAOImpl")
 public class UserDAOImpl implements UserDAO {
 
+    private DataSource dataSource;
     public void save(User U) {
+        try {
+            Connection connection = dataSource.getConnection();
+            Random rand = new Random();
+            int id  = 100 + rand.nextInt((100 - 5) + 1);
+            connection.createStatement().executeUpdate("insert into user VALUES ("+id+",'Tao Li')");
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         System.out.println("User Saved!");
-        throw new RuntimeException("Exception fot test!");
+//        throw new RuntimeException("Exception fot test!");
     }
 
     public void delete(User U) {
         System.out.println("User Deleted!");
     }
-    
 
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    @Resource
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 }
